637dd070b2c277d78ec7aeed459d113542d9cb2a
Dennis <dshkrebe@smail.uni-koeln.de>
2025-06-29 14:44:20 +0200

    rf

--- Web-sites/Projekt/js/javascript.js
+++ Web-sites/Projekt/js/javascript.js
@@ -1,3 +1,51 @@
+function quizbutton(){
+  if(document.getElementById("quizbutton").innerHTML === "Starten"){
+    console.log("test");
+    document.getElementById("quizbutton").innerText = "Auswertung";
+    document.getElementById("questions").style.display = "block";
+    document.getElementById("quiztitel").innerHTML = "Abgabe:";
+  }else if (document.getElementById("quizbutton").innerHTML === "Auswertung"){
+
+  }else{
+
+  }
+}
+
+
+function rangeValue(){
+  document.getElementById("rangeValue").innerHTML = document.getElementById("range1").value;
+}
+
+function neueSchriftgroesse(){
+  let size = document.getElementById("range1").value + "px";
+
+  //Klassennamen geben ein Array zurück
+  let blockArray = document.getElementsByClassName("block");
+  for (let i = 0; i < blockArray.length; i++) {
+    blockArray[i].style.fontSize = size;
+  }
+
+  let articles = document.getElementsByTagName("article");
+  for (let i = 0; i < articles.length; i++) {
+    articles[i].style.fontSize = size;
+  }
+}
+
+function design(){
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
 //w3Schools Accordion
 var acc = document.getElementsByClassName("accordion");
 var i;
@@ -17,5 +65,3 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 } 
-
-
